2e552362d56e8f42d977a5d547e26d3e829b14ba
alishagarric <alisha@laxaltandmciver.co>
2022-12-02 12:20:34 -0600

    :rocket: deploying: styles progress on multicolumn

--- assets/global.js
+++ assets/global.js
@@ -14903,6 +14903,61 @@ var FeaturedBlog = /*#__PURE__*/function (_HTMLElement3) {
 }( /*#__PURE__*/_wrapNativeSuper(HTMLElement));
 
 customElements.define("featured-blog", FeaturedBlog);
+/* Multicolumn Logic */
+///////////////////////////////
+///////////////////////////////
+
+var Multicolumn = /*#__PURE__*/function (_HTMLElement4) {
+  _inherits(Multicolumn, _HTMLElement4);
+
+  var _super4 = _createSuper(Multicolumn);
+
+  function Multicolumn() {
+    var _this9;
+
+    _classCallCheck(this, Multicolumn);
+
+    _this9 = _super4.call(this);
+
+    if (_this9.dataset.slider == "true") {
+      _this9.slides = _assertThisInitialized(_this9);
+      theme.initWhenVisible({
+        element: _assertThisInitialized(_this9),
+        callback: _this9.init.bind(_assertThisInitialized(_this9)),
+        threshold: 600
+      });
+    }
+
+    return _this9;
+  }
+
+  _createClass(Multicolumn, [{
+    key: "init",
+    value: function init() {
+      var _this10 = this;
+
+      setTimeout(function () {
+        _this10.flickity = new Flickity(_this10.slides, {
+          accessibility: false,
+          rightToLeft: theme.config.rtl,
+          prevNextButtons: true,
+          arrowShape: "M38.39,17.65a3.91,3.91,0,0,0-1.12-1.51,3.83,3.83,0,0,0-1.69-.8A3.84,3.84,0,0,0,32.1,16.4L1.33,47.17a3.83,3.83,0,0,0-.83,4.2,3.85,3.85,0,0,0,.83,1.25L32.1,83.38a3.81,3.81,0,0,0,2.72,1.13,3.85,3.85,0,0,0,2.73-6.57L13.34,53.74h83a3.85,3.85,0,1,0,0-7.69h-83l24.21-24.2a3.8,3.8,0,0,0,1.05-2A3.86,3.86,0,0,0,38.39,17.65Z",
+          pageDots: true,
+          wrapAround: false,
+          cellAlign: "left",
+          selectedAttraction: 0.2,
+          autoPlay: false,
+          friction: 0.8,
+          adaptiveHeight: false
+        });
+      });
+    }
+  }]);
+
+  return Multicolumn;
+}( /*#__PURE__*/_wrapNativeSuper(HTMLElement));
+
+customElements.define("multicolumn-slider", Multicolumn);
 /* Logic the Shopify Starter Included */
 ////////////////////////////////////////
 ///////////////////////////////////////
@@ -14921,7 +14976,7 @@ document.querySelectorAll(".disclosure").forEach(function (details) {
 });
 
 function debounce(fn, wait) {
-  var _this9 = this;
+  var _this11 = this;
 
   var t;
   return function () {
@@ -14931,7 +14986,7 @@ function debounce(fn, wait) {
 
     clearTimeout(t);
     t = setTimeout(function () {
-      return fn.apply(_this9, args);
+      return fn.apply(_this11, args);
     }, wait);
   };
 }
