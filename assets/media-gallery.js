018e88971e446f7ff1460199c6d2f41bd09a9f03
alishagarric <alisha@laxaltandmciver.co>
2023-03-15 16:26:09 -0500

    :rocket: deploying: media slider logic

--- assets/media-gallery.js
+++ assets/media-gallery.js
@@ -5,12 +5,36 @@ if (!customElements.get("media-gallery")) {
       constructor() {
         super();
 
-        this.dots = this.querySelectorAll(`.main-product__media__dots li`);
+        this.dots = this.querySelectorAll(`.main-product__media__dots__dot`);
+        this.prev = this.querySelector(`.main-product__media__dots__prev`);
+        this.next = this.querySelector(`.main-product__media__dots__next`);
+
+        if (!this.dots.length != 0 || !this.prev || !this.next) return;
+
+        this.setupListener();
       }
 
-      init() {
-        //TODO: why isn't this firing
-        this.dots.addEventListener("click", console.log("click", this.dots));
+      setupListener() {
+        this.dots.forEach((dot) => {
+          dot.addEventListener("click", () => {
+            console.log(dot.dataset);
+            this.setActiveMedia(`${dot.dataset.mediaDotId}`);
+          });
+        });
+
+        this.prev.addEventListener("click", () => {
+          const activeDot = this.querySelector(
+            `.main-product__media__dots__dot.__active`
+          );
+          this.setActiveMedia(activeDot.previousSibling.dataset.mediaDotId);
+        });
+
+        this.next.addEventListener("click", () => {
+          const activeDot = this.querySelector(
+            `.main-product__media__dots__dot.__active`
+          );
+          this.setActiveMedia(activeDot.nextSibling.dataset.mediaDotId);
+        });
       }
 
       setActiveMedia(mediaId) {
@@ -18,6 +42,21 @@ if (!customElements.get("media-gallery")) {
         if (!activeMedia) return;
 
         activeMedia.parentElement.prepend(activeMedia);
+        this.setActiveDot(mediaId);
+      }
+
+      setActiveDot(mediaDotId) {
+        const activeDot = this.querySelector(
+          `[data-media-dot-id="${mediaDotId}"]`
+        );
+        if (!activeDot) return;
+
+        activeDot.classList.add("__active");
+        this.dots.forEach((dot) => {
+          if (dot != activeDot) {
+            dot.classList.remove("__active");
+          }
+        });
       }
     }
   );
