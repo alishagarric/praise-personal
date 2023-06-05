d7030006723a88fe891f07fb5ff64e00778df93c
alishagarric <alisha@laxaltandmciver.co>
2023-06-05 10:17:42 -0500

    :rocket: deploying: remove old gift card error logic

--- assets/recipient-form.js
+++ assets/recipient-form.js
@@ -4,7 +4,6 @@ if (!customElements.get("recipient-form")) {
     class RecipientForm extends HTMLElement {
       constructor() {
         super();
-        console.log("constructor");
         this.checkboxInput = this.querySelector(
           `#Recipient-Checkbox-${this.dataset.sectionId}`
         );
@@ -22,13 +21,6 @@ if (!customElements.get("recipient-form")) {
         this.messageInput = this.querySelector(
           `#Recipient-message-${this.dataset.sectionId}`
         );
-        this.errorMessageWrapper = this.querySelector(
-          `#Recipient-errors-${this.dataset.sectionId}`
-        );
-        this.errorMessageList = this.errorMessageWrapper?.querySelector("ul");
-        this.errorMessage =
-          this.errorMessageWrapper?.querySelector(".message-error");
-        this.defaultErrorHeader = this.errorMessage?.innerText;
         this.currentProductVariantId = this.dataset.productVariantId;
         this.validEmail = false;
         this.emailInput.addEventListener("input", this.emailChange.bind(this));
@@ -56,7 +48,6 @@ if (!customElements.get("recipient-form")) {
       onChange() {
         if (!this.checkboxInput.checked) {
           this.clearInputFields();
-          this.clearErrorMessage();
           this.classList.add("__hide-fields");
           this.setAvailable();
           this.validEmail = false;
@@ -99,77 +90,6 @@ if (!customElements.get("recipient-form")) {
         if (!addButton) return;
         addButtonText.textContent = window.variantStrings.addToCart;
         addButton.removeAttribute("disabled");
-      }
-
-      displayErrorMessage(title, body) {
-        console.log("displayErrorMessage");
-        this.clearErrorMessage();
-        this.errorMessageWrapper.hidden = false;
-        if (typeof body === "object") {
-          this.errorMessage.innerText = this.defaultErrorHeader;
-          return Object.entries(body).forEach(([key, value]) => {
-            const errorMessageId = `RecipientForm-${key}-error-${this.dataset.sectionId}`;
-            const fieldSelector = `#Recipient-${key}-${this.dataset.sectionId}`;
-            const placeholderElement = this.querySelector(`${fieldSelector}`);
-            const label =
-              placeholderElement?.getAttribute("placeholder") || key;
-            const message = `${label} ${value}`;
-            const errorMessageElement = this.querySelector(
-              `#${errorMessageId}`
-            );
-            const errorTextElement =
-              errorMessageElement?.querySelector(".message-error");
-            if (!errorTextElement) return;
-
-            if (this.errorMessageList) {
-              this.errorMessageList.appendChild(
-                this.createErrorListItem(fieldSelector, message)
-              );
-            }
-
-            errorTextElement.innerText = `${message}.`;
-            errorMessageElement.classList.remove("hidden");
-
-            const inputElement = this[`${key}Input`];
-            if (!inputElement) return;
-
-            inputElement.setAttribute("aria-invalid", true);
-            inputElement.setAttribute("aria-describedby", errorMessageId);
-          });
-        }
-
-        this.errorMessage.innerText = body;
-      }
-
-      createErrorListItem(target, message) {
-        console.log("createErrorListItem");
-        const li = document.createElement("li");
-        const a = document.createElement("a");
-        a.setAttribute("href", target);
-        a.innerText = message;
-        li.appendChild(a);
-        li.className = "message-error";
-        return li;
-      }
-
-      clearErrorMessage() {
-        console.log("clearErrorMessage");
-        this.errorMessageWrapper.hidden = true;
-
-        if (this.errorMessageList) this.errorMessageList.innerHTML = "";
-
-        this.querySelectorAll(".recipient-fields__error").forEach((field) => {
-          field.classList.add("hidden");
-          const textField = field.querySelector(".message-error");
-          if (textField) textField.innerText = "";
-        });
-
-        [this.emailInput, this.messageInput, this.nameInput].forEach(
-          (inputElement) => {
-            inputElement.setAttribute("aria-invalid", false);
-            inputElement.removeAttribute("aria-describedby");
-          }
-        );
       }
     }
   );
